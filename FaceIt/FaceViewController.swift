5f33fbceb7494b5af3fac537fe3c36a7d7258001
Liuyubao <523331232@qq.com>
2018-01-23 16:55:24 +0800

    upload all

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by liuyubao on 2017/7/28.
+//  Copyright © 2017年 liuyubao. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    @IBOutlet weak var faceView: FaceView!{
+        didSet{
+            let handler = #selector(faceView.changeScale(byReactingTO:))
+            let pinchRecognizer = UIPinchGestureRecognizer(target: faceView,action: handler)
+            faceView.addGestureRecognizer(pinchRecognizer)
+//            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.toggleEyes(byReactingTo: )))
+//            tapRecognizer.numberOfTapsRequired = 1
+//            faceView.addGestureRecognizer(tapRecognizer)
+            let swipeUpRecognizer = UISwipeGestureRecognizer(target:self, action:#selector(increaseHappiness))
+            swipeUpRecognizer.direction = .up
+            faceView.addGestureRecognizer(swipeUpRecognizer)
+            let swipeDownRecognizer = UISwipeGestureRecognizer(target:self, action:#selector(decreaseHappiness))
+            swipeDownRecognizer.direction = .down
+            faceView.addGestureRecognizer(swipeDownRecognizer)
+            
+            updateUI()
+                        
+            
+        }
+    }
+    
+    private struct HeadShake{
+        static let angle = CGFloat.pi/6
+        static let segmentDuration: TimeInterval = 0.5
+    }
+    
+    private func rotateFace(by angle: CGFloat){
+        faceView.transform = faceView.transform.rotated(by: angle)
+        
+    }
+    
+    private func shakeHead(){
+        UIView.animate(withDuration: HeadShake.segmentDuration,
+                       animations: { self.rotateFace(by: HeadShake.angle)},
+                       completion: {finished in
+                        if finished{
+                            UIView.animate(
+                                withDuration: HeadShake.segmentDuration,
+                                animations: {self.rotateFace(by: -HeadShake.angle*2)},
+                                completion: { finished in
+                                    UIView.animate(
+                                        withDuration: HeadShake.segmentDuration,
+                                        animations: {self.rotateFace(by: HeadShake.angle)}
+                            )
+                        }
+                    )
+                }
+            }
+        )
+    }
+    
+    
+    
+    @IBAction func shakeHead(_ sender: UITapGestureRecognizer) {
+        shakeHead()
+    }
+    
+    func increaseHappiness(){
+        expression = expression.happier
+    }
+    func decreaseHappiness(){
+        expression = expression.sadder
+    }
+    
+    func toggleEyes(byReactingTo tapRecognizer: UITapGestureRecognizer){
+        if tapRecognizer.state == .ended{
+            let eyes: FacialExpression.Eyes = (expression.eyes == .closed) ? .open : .closed
+            expression = FacialExpression(eyes: eyes, mouth:expression.mouth)
+        }
+    }
+    
+    var expression = FacialExpression(eyes: .closed, mouth: .frown){
+        didSet{
+            updateUI()
+            
+        }
+    }
+    
+    
+    private func updateUI(){
+        switch expression.eyes{
+        case .open:
+            faceView?.eyesOpen = true
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.grin: 0.5, .frown: -1.0, .smile:1.0, .neutral:0.0, .smirk: -0.5]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
+
